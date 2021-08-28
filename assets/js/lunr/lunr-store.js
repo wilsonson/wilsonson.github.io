a5f6cea70f1c16c2b54538f0aedf21413401c817
wilsonson <7401913+wilsonson@users.noreply.github.com>
2021-08-29 00:29:57 +0800

    First update

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "New blog!",
+        "excerpt":"After many years of coding, I decided to write down some notes for myself. Hopefully, these experiences can also help others.  ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/new-blog/",
+        "teaser": null
+      },{
+        "title": "Performance Test",
+        "excerpt":"Most developers should have the experience to be asked by the senior or other business stakeholders for conducting load tests. The so-called load test is all about performance, and it’s aimed to find out all the limitations/bottlenecks of an application. When we assess the performance of an application, there’re a...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/performance-test/",
+        "teaser": null
+      }]
